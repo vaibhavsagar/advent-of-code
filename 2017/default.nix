f79354c8dda4b1274b386dd10b6d42c994472468
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-27 00:39:31 +0800

    Add utility library

--- 2017/default.nix
+++ 2017/default.nix
@@ -4,6 +4,7 @@ let
   IHaskell = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).ihaskell;
   # ./updater versions.json nixpkgs nixos-17.09
   pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
+  my-lib   = (import pinned {}).pkgs.haskellPackages.callCabal2nix "lib" ./lib {};
 in import "${IHaskell}/release.nix" {
   nixpkgs = import pinned {};
   packages = self: with self; [
@@ -12,6 +13,7 @@ in import "${IHaskell}/release.nix" {
     containers
     directory
     filepath
+    my-lib
     utf8-string
   ];
 }

fcd203261d1f0601fd4ef1f74890ad4fa6988ebd
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-28 16:21:53 +0800

    Don't haddock lib to prevent issues when empty

--- 2017/default.nix
+++ 2017/default.nix
@@ -5,7 +5,7 @@ let
   # ./updater versions.json nixpkgs nixos-17.09
   pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
   nixpkgs  = import pinned {};
-  my-lib   = nixpkgs.haskellPackages.callCabal2nix "lib" ./lib {};
+  my-lib   = nixpkgs.haskell.lib.dontHaddock (nixpkgs.haskellPackages.callCabal2nix "lib" ./lib {});
 in import "${IHaskell}/release.nix" {
   inherit nixpkgs;
   packages = self: with self; [

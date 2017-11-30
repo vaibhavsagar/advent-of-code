093b80b6df7762585b11d06015d272d8d1803596
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-30 17:57:51 +0800

    Simplify default.nix further

--- 2017/default.nix
+++ 2017/default.nix
@@ -1,14 +1,12 @@
 let
   inherit (import <nixpkgs> {}) fetchFromGitHub lib;
+  # ./updater versions.json ihaskell
+  # ./updater versions.json nixpkgs nixos-17.09
   versions = lib.mapAttrs
     (_: fetchFromGitHub)
     (builtins.fromJSON (builtins.readFile ./versions.json));
-  # ./updater versions.json ihaskell
-  IHaskell = versions.ihaskell;
-  # ./updater versions.json nixpkgs nixos-17.09
-  pinned   = versions.nixpkgs;
-  nixpkgs  = import pinned {};
-in import "${IHaskell}/release.nix" {
+  nixpkgs  = import versions.nixpkgs {};
+in import "${versions.ihaskell}/release.nix" {
   inherit nixpkgs;
   packages = self: with self; [
     attoparsec

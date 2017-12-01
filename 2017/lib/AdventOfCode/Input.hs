31516b4fc77e83ed4364cf148e31cc05ddbb14d8
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-01 13:34:39 +0800

    Add dayString helper

--- 2017/lib/AdventOfCode/Input.hs
+++ 2017/lib/AdventOfCode/Input.hs
@@ -4,8 +4,12 @@ module AdventOfCode.Input where
 
 import qualified Data.ByteString                  as B
 import qualified Data.ByteString.Char8            as B (lines)
+import qualified Data.ByteString.UTF8             as BU
 import qualified Data.Attoparsec.ByteString       as A
 import qualified Data.Attoparsec.ByteString.Char8 as AC
 
 day :: Int -> IO [B.ByteString]
 day n = B.lines <$> B.readFile ("../input/day" ++ show n ++ ".txt")
+
+dayString :: Int -> IO [String]
+dayString = fmap (map BU.toString) . day

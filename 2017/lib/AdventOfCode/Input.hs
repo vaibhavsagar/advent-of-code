be0827841a676ce05fc224bf5af9bff20245b09a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-01 19:07:00 +0800

    Add various input functions and adapt Day1

--- 2017/lib/AdventOfCode/Input.hs
+++ 2017/lib/AdventOfCode/Input.hs
@@ -8,8 +8,14 @@ import qualified Data.ByteString.UTF8             as BU
 import qualified Data.Attoparsec.ByteString       as A
 import qualified Data.Attoparsec.ByteString.Char8 as AC
 
-day :: Int -> IO [B.ByteString]
-day n = B.lines <$> B.readFile ("../input/day" ++ show n ++ ".txt")
+day :: Int -> IO B.ByteString
+day n = B.init <$> B.readFile ("../input/day" ++ show n ++ ".txt")
 
-dayString :: Int -> IO [String]
-dayString = fmap (map BU.toString) . day
+dayString :: Int -> IO String
+dayString = fmap BU.toString . day
+
+dayLines :: Int -> IO [B.ByteString]
+dayLines = fmap B.lines . day
+
+dayLinesString :: Int -> IO [String]
+dayLinesString = fmap (map BU.toString) . dayLines

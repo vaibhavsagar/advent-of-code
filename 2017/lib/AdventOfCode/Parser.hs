7aa67b505f795b23c59694b5abedaad278b5926d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-09 15:18:19 +1100

    lib: don't use deprecated Char8 module of attoparsec

--- 2017/lib/AdventOfCode/Parser.hs
+++ 2017/lib/AdventOfCode/Parser.hs
@@ -1,11 +1,12 @@
 module AdventOfCode.Parser where
 
-import qualified Data.Attoparsec.ByteString.Char8 as A
-import qualified Data.ByteString                  as B
+import qualified Data.Attoparsec.ByteString as A
+import qualified Data.ByteString            as B
+import           Data.Char (ord)
 
 import AdventOfCode.Helpers ((.:))
 
 parsed = either error id .: A.parseOnly
 
 tillSpace :: A.Parser B.ByteString
-tillSpace = A.takeWhile (/=' ')
+tillSpace = A.takeWhile (/=(fromIntegral $ ord ' '))

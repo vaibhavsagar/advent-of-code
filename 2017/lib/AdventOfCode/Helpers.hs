708f3a13f99e0a77f2d3e7c694b57f25fae94e20
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-12 13:35:40 +0800

    Day 12: add ordNub to lib and refactor

--- 2017/lib/AdventOfCode/Helpers.hs
+++ 2017/lib/AdventOfCode/Helpers.hs
@@ -1,10 +1,19 @@
 module AdventOfCode.Helpers
     ( (&)
     , (.:)
+    , ordNub
     ) where
 
-import Data.Function ((&))
+import           Data.Function ((&))
+import qualified Data.Set as Set
 
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (.:) = (.).(.)
 
+ordNub :: (Ord a) => [a] -> [a]
+ordNub l = go Set.empty l
+    where
+        go _ [] = []
+        go s (x:xs) = if x `Set.member` s
+            then go s xs
+            else x : go (Set.insert x s) xs

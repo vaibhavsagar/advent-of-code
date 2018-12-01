1df800037f5d7828fbc45ded57865dcc73c5f574
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-11-30 21:40:56 -0500

    2018: init

---
+++ 2018/default.nix
@@ -0,0 +1,30 @@
+let
+  fetcher = { owner, repo, rev, sha256, ... }: builtins.fetchTarball {
+    inherit sha256;
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+  };
+  inherit (import <nixpkgs> {}) lib;
+  # ./updater versions.json ihaskell
+  # ./updater versions.json nixpkgs nixos-17.09
+  versions = lib.mapAttrs
+    (_: fetcher)
+    (builtins.fromJSON (builtins.readFile ./versions.json));
+  nixpkgs = import versions.nixpkgs {};
+in import "${versions.ihaskell}/release.nix" {
+  inherit nixpkgs;
+  compiler = "ghc844";
+  packages = self: with self; [
+    attoparsec
+    bytestring
+    containers
+    directory
+    fgl
+    filepath
+    lens
+    memoize
+    split
+    unordered-containers
+    utf8-string
+    vector
+  ] ++ nixpkgs.lib.singleton (import ./lib);
+}
---
+++ 2018/lib/AdventOfCode/Input.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module AdventOfCode.Input where
+
+import qualified Data.ByteString                  as B
+import qualified Data.ByteString.Char8            as B (lines)
+import qualified Data.ByteString.UTF8             as BU
+import qualified Data.Attoparsec.ByteString       as A
+import qualified Data.Attoparsec.ByteString.Char8 as AC
+
+day :: Int -> IO B.ByteString
+day n = B.init <$> B.readFile ("../input/day" ++ show n ++ ".txt")
+
+dayString :: Int -> IO String
+dayString = fmap BU.toString . day
+
+dayLines :: Int -> IO [B.ByteString]
+dayLines = fmap B.lines . day
+
+dayLinesString :: Int -> IO [String]
+dayLinesString = fmap (map BU.toString) . dayLines
+
+readInt :: String -> Int
+readInt = read

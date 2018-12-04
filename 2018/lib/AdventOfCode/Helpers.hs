3ae9eacceab8953fc980633e3a99bec338194d27
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-12-04 10:22:30 -0500

    AdventOfCode.Helpers: add freqs

--- 2018/lib/AdventOfCode/Helpers.hs
+++ 2018/lib/AdventOfCode/Helpers.hs
@@ -1,17 +1,23 @@
 module AdventOfCode.Helpers
     ( (&)
     , (.:)
+    , freqs
     , ordNub
     ) where
 
 import           Data.Function ((&))
+import           Data.List (foldl')
+import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (.:) = (.).(.)
 
+freqs :: Ord a => [a] -> Map.Map a Int
+freqs = foldl' (\m a -> Map.insertWith (+) a 1 m) Map.empty
+
 ordNub :: (Ord a) => [a] -> [a]
-ordNub l = go Set.empty l
+ordNub = go Set.empty
     where
         go _ [] = []
         go s (x:xs) = if x `Set.member` s
